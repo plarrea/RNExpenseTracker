49451dc6f87091ac1b7ce57e6f054f4eade8825f
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-06 18:07:23 -0500

    expenses output components

---
+++ src/components/ExpensesOutput/ExpensesList.js
@@ -0,0 +1,18 @@
+import { FlatList } from 'react-native';
+import ExpenseItem from './ExpenseItem';
+
+const renderExpenseItem = (itemData) => {
+  return <ExpenseItem {...itemData.item} />;
+};
+
+const ExpensesList = ({ expenses }) => {
+  return (
+    <FlatList
+      data={expenses}
+      keyExtractor={(item) => item.id}
+      renderItem={renderExpenseItem}
+    />
+  );
+};
+
+export default ExpensesList;
