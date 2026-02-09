72353d43580aa3fdae58b13449ce7cb7224b5878
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-09 13:53:05 -0500

    expenses output changes

--- src/components/ExpensesOutput/ExpenseItem.js
+++ src/components/ExpensesOutput/ExpenseItem.js
@@ -1,10 +1,22 @@
+import { useNavigation } from '@react-navigation/native';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { GlobalStyles } from '../../constants/styles';
 import { getFormattedDate } from '../../utils/date';
+const ExpenseItem = ({ id, description, amount, date }) => {
+  const navigation = useNavigation();
 
-const ExpenseItem = ({ description, amount, date }) => {
+  const expensePressHandler = () => {
+    navigation.navigate('ManageExpense', {
+      expenseId: id,
+    });
+  };
+
   return (
-    <Pressable>
+    <Pressable
+      android_ripple={true}
+      onPress={expensePressHandler}
+      style={({ pressed }) => pressed && styles.pressed}
+    >
       <View style={styles.expenseItem}>
         <View>
           <Text style={[styles.textBase, styles.description]}>
@@ -23,6 +35,9 @@ const ExpenseItem = ({ description, amount, date }) => {
 export default ExpenseItem;
 
 const styles = StyleSheet.create({
+  pressed: {
+    opacity: 0.75,
+  },
   expenseItem: {
     padding: 12,
     marginVertical: 8,
--- src/components/ExpensesOutput/ExpensesOutput.js
+++ src/components/ExpensesOutput/ExpensesOutput.js
@@ -3,74 +3,11 @@ import { GlobalStyles } from '../../constants/styles';
 import ExpensesList from './ExpensesList';
 import ExpensesSumary from './ExpensesSumary';
 
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    description: 'A pair of shoes',
-    amount: 59.99,
-    date: new Date('2021-12-10'),
-  },
-  {
-    id: 'e2',
-    description: 'A pair of trousers',
-    amount: 89.29,
-    date: new Date('2022-01-05'),
-  },
-  {
-    id: 'e3',
-    description: 'Some bananas',
-    amount: 5.99,
-    date: new Date('2021-12-05'),
-  },
-  {
-    id: 'e4',
-    description: 'A book',
-    amount: 14.99,
-    date: new Date('2022-02-19'),
-  },
-  {
-    id: 'e5',
-    description: 'Another book',
-    amount: 18.99,
-    date: new Date('2022-02-20'),
-  },
-  {
-    id: 'e6',
-    description: 'A pair of shoes',
-    amount: 59.99,
-    date: new Date('2021-12-10'),
-  },
-  {
-    id: 'e7',
-    description: 'A pair of trousers',
-    amount: 89.29,
-    date: new Date('2022-01-05'),
-  },
-  {
-    id: 'e8',
-    description: 'Some bananas',
-    amount: 5.99,
-    date: new Date('2021-12-05'),
-  },
-  {
-    id: 'e9',
-    description: 'A book',
-    amount: 14.99,
-    date: new Date('2022-02-19'),
-  },
-  {
-    id: 'e10',
-    description: 'Another book',
-    amount: 18.99,
-    date: new Date('2022-02-20'),
-  },
-];
-
 const ExpensesOutput = ({ expensesPeriod, expenses }) => {
   return (
     <View style={styles.container}>
-      <ExpensesSumary periodName={expensesPeriod} expenses={DUMMY_EXPENSES} />
-      <ExpensesList expenses={DUMMY_EXPENSES} />
+      <ExpensesSumary periodName={expensesPeriod} expenses={expenses} />
+      <ExpensesList expenses={expenses} />
     </View>
   );
 };
