927dd713454fc2cbf75ed614a0ec071549cafbf3
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-09 13:53:40 -0500

    implement context use in screens

--- src/screens/AllExpenses.js
+++ src/screens/AllExpenses.js
@@ -1,7 +1,10 @@
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
+import useExpenses from '../store/use-expenses';
 
 const AllExpenses = () => {
-  return <ExpensesOutput expensesPeriod="Total" />;
+  const { expenses } = useExpenses();
+
+  return <ExpensesOutput expensesPeriod="Total" expenses={expenses} />;
 };
 
 export default AllExpenses;
--- src/screens/RecentExpenses.js
+++ src/screens/RecentExpenses.js
@@ -1,7 +1,16 @@
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
+import useExpenses from '../store/use-expenses';
+import { getDateMinusDay } from '../utils/date';
 
 const RecentExpenses = () => {
-  return <ExpensesOutput expensesPeriod="Last 7 Days" />;
+  const { expenses } = useExpenses();
+
+  const recent = expenses.filter((expense) => {
+    const sevenDaysAgo = getDateMinusDay(new Date(), 7);
+    return expense.date > sevenDaysAgo;
+  });
+
+  return <ExpensesOutput expensesPeriod="Last 7 Days" expenses={recent} />;
 };
 
 export default RecentExpenses;
